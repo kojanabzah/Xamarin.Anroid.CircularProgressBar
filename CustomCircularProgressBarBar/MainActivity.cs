66e615521f22e57c02d4f051bfa5e5fe1711e31a
klasj\kabzah <Kojan.Abzah@kla-tencor.com>
2017-07-12 15:58:06 +0300

    Add project files.

---
+++ CustomCircularProgressBarBar/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace CustomCircularProgressBarBar
+{
+    [Activity(Label = "CustomCircularProgressBarBar", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
